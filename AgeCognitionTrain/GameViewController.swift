07bc19e56b18b5d5a3e35f3e3561aa002dc51c08
HankerLu <hankerlu1996@gmail.com>
2024-05-17 20:52:03 +0800

    NEW: add image view

--- AgeCognitionTrain/GameViewController.swift
+++ AgeCognitionTrain/GameViewController.swift
@@ -11,9 +11,12 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var speakerImageView: UIImageView!
+    @IBOutlet weak var lineImageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // 设置背景颜色为白色
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
@@ -25,10 +28,15 @@ class GameViewController: UIViewController {
                 view.presentScene(scene)
             }
             
-            view.ignoresSiblingOrder = true
+            view.ignoresSiblingOrder = false
+            view.showsFPS = false
+            view.showsNodeCount = false
             
-            view.showsFPS = true
-            view.showsNodeCount = true
+            //设置imageView的屏幕大小
+            // imageView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.height, height: 500)
+            imageView.image = UIImage(named: "memory2")
+            speakerImageView.image = UIImage(named: "speaker")
+            lineImageView.image = UIImage(named: "line")
         }
     }
 
