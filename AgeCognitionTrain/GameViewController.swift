6da4ffcdaab0d445608cee8c44d3c8a12876410a
HankerLu <hankerlu1996@gmail.com>
2024-05-17 19:37:58 +0800

    NEW: init cognitiontrain project

--- AgeCognitionTrain/GameViewController.swift
+++ AgeCognitionTrain/GameViewController.swift
@@ -13,7 +13,8 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        // 设置背景颜色为白色
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
