ec9309d63424a81bd63a67a794fd8d403e211100
HankerLu <hankerlu1996@gmail.com>
2024-05-20 19:57:04 +0800

    NEW: add sentence long press print

--- AgeCognitionTrain/GameViewController.swift
+++ AgeCognitionTrain/GameViewController.swift
@@ -13,16 +13,43 @@ class GameViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var lineImageView: UIImageView!
+    @IBOutlet weak var editorImageView: UIImageView!
     @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var editorTextView: UITextView!
     
     @IBOutlet weak var longPressGestureRecognizer: UILongPressGestureRecognizer!
+    
     var isLongPressActive = false
     var timer: Timer?
+    var globalIndex = 0 // 全局变量来跟踪当前的索引位置
+ 
+    let mixedString = "你好，Hello，世界！World！"
+    var lengthIndex: Int = 0
+
+    func getNextCharacter(from string: String) -> String? {
+        // 如果全局索引已经超出字符串长度，返回nil
+        if globalIndex >= string.count {
+            return nil
+        }
+        
+        // 获取当前索引位置的字符
+        let character = string[string.index(string.startIndex, offsetBy: globalIndex)]
+        
+        // 将全局索引位置向前移动
+        globalIndex += 1
+        
+        return String(character)
+    }
 
     @objc func longPressAction() {
         if isLongPressActive {
             // 在这里执行长按时需要持续进行的操作
             print("longPressAction is being executed.")
+            if globalIndex >= lengthIndex {
+                globalIndex = 0
+            }
+            let ret = getNextCharacter(from: mixedString)!
+            editorTextView.text += ret
         }
     }
 
@@ -65,8 +92,10 @@ class GameViewController: UIViewController {
             // imageView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.height, height: 500)
             imageView.image = UIImage(named: "memory2")
             lineImageView.image = UIImage(named: "line")
+            editorImageView.image = UIImage(named: "editor")
             recordButton.imageView?.image = UIImage(named: "speaker")
             recordButton.addGestureRecognizer(longPressGestureRecognizer)
+            lengthIndex = mixedString.count
         }
     }
 
